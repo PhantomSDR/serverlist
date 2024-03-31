392ca9b616c594b8dbbc94f58b4fbf5b28292d7f
Jie Feng <hjf3108@gmail.com>
2024-03-31 14:28:40 +0800

    Add trace access logging

--- src/main.rs
+++ src/main.rs
@@ -4,34 +4,46 @@ mod servers;
 use std::net::SocketAddr;
 
 use axum::{
-    http::{HeaderValue, Method}, routing::{get, post}, Router
+    http::{HeaderValue, Method},
+    routing::{get, post},
+    Router,
 };
 use axum_client_ip::SecureClientIpSource;
-use tower_http::{add_extension::AddExtensionLayer, cors::{Any, CorsLayer}};
+use tower_http::{
+    add_extension::AddExtensionLayer,
+    cors::{Any, CorsLayer},
+    trace::TraceLayer,
+};
 
 use api::{add_server, get_all_servers};
 use servers::SharedServerList;
 
 #[tokio::main]
 async fn main() {
-    tracing_subscriber::fmt::init();
+    tracing_subscriber::fmt()
+        .with_max_level(tracing::Level::DEBUG)
+        .init();
 
     // Only allow phantomsdr.github.io to access the API
-    let cors = CorsLayer::new()
-        .allow_methods([Method::GET, Method::POST]);
-    
+    let cors = CorsLayer::new().allow_methods([Method::GET, Method::POST]);
+
     // if DEBUG env is true, allow all origins
     let cors = match std::env::var("DEBUG") {
         Ok(_) => cors.allow_origin(Any),
-        _ => cors.allow_origin("https://phantomsdr.github.io".parse::<HeaderValue>().unwrap())
+        _ => cors.allow_origin(
+            "https://phantomsdr.github.io"
+                .parse::<HeaderValue>()
+                .unwrap(),
+        ),
     };
-    
+
     let app = Router::new()
         .route("/api/v1/ping", post(add_server))
         .route("/api/v1/all", get(get_all_servers))
         .layer(SecureClientIpSource::ConnectInfo.into_extension())
         .layer(cors)
-        .layer(AddExtensionLayer::new(SharedServerList::default()));
+        .layer(AddExtensionLayer::new(SharedServerList::default()))
+        .layer(TraceLayer::new_for_http());
 
     let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
     axum::serve(
