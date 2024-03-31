f45298415791b32721406e562f2cc746680f34fd
Jie Feng <hjf3108@gmail.com>
2024-03-31 13:08:48 +0800

    Allow CORS from github page

--- src/main.rs
+++ src/main.rs
@@ -4,11 +4,10 @@ mod servers;
 use std::net::SocketAddr;
 
 use axum::{
-    routing::{get, post},
-    Router,
+    http::{HeaderValue, Method}, routing::{get, post}, Router
 };
 use axum_client_ip::SecureClientIpSource;
-use tower_http::add_extension::AddExtensionLayer;
+use tower_http::{add_extension::AddExtensionLayer, cors::CorsLayer};
 
 use api::{add_server, get_all_servers};
 use servers::SharedServerList;
@@ -17,10 +16,16 @@ use servers::SharedServerList;
 async fn main() {
     tracing_subscriber::fmt::init();
 
+    // Only allow phantomsdr.github.io to access the API
+    let cors = CorsLayer::new()
+        .allow_methods([Method::GET, Method::POST])
+        .allow_origin("https://phantomsdr.github.io".parse::<HeaderValue>().unwrap());
+    
     let app = Router::new()
         .route("/api/v1/ping", post(add_server))
         .route("/api/v1/all", get(get_all_servers))
         .layer(SecureClientIpSource::ConnectInfo.into_extension())
+        .layer(cors)
         .layer(AddExtensionLayer::new(SharedServerList::default()));
 
     let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
